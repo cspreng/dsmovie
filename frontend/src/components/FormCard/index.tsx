2b070e40d137a6c4dab3c65b537dcdb7ed06efd6
Carlos Eduardo Spreng <carlos.spreng@gmail.com>
2022-01-16 20:47:49 -0300

    Pagination

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Movie } from 'types/movie';
 import { BASE_URL } from 'utils/requests';
 import './styles.css';
